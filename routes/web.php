9dac6989c2c2ee163c2ce652b1b59016b0732726
Long Kim <kimlongphp110499>
2023-09-06 13:30:28 +0700

    chat

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,7 @@ Route::prefix('orders')->name('orders.')->group(function () {
 
 
 
+Route::get('/', 'App\Http\Controllers\PusherController@index');
+Route::post('/broadcast', 'App\Http\Controllers\PusherController@broadcast');
+Route::post('/receive', 'App\Http\Controllers\PusherController@receive');
+
